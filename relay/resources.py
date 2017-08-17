860bcb337a09c20d75f14e1f292969bb8d742146
dakingha69 <dongha.kim210@gmail.com>
2017-08-17 13:39:17 +0200

    Add /contacts endpoint

--- relay/resources.py
+++ relay/resources.py
@@ -53,8 +53,11 @@ class User(Resource):
 
 class ContactList(Resource):
 
-    def get(self, todo_id):
-        pass
+    def __init__(self, trustlines):
+        self.trustlines = trustlines
+
+    def get(self, network_address, user_address):
+        return self.trustlines.currency_network_graphs[network_address].get_friends(user_address)
 
 
 class TrustlineList(Resource):
