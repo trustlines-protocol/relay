7a7912f786edb432370a6ca84ba19067b866cafd
Bernd Bohmeier <berndbohmeier@googlemail.com>
2018-04-09 00:43:27 +0200

    Tr fix build

--- .travis/smoketest.sh
+++ .travis/smoketest.sh
@@ -5,8 +5,11 @@ COMPOSE_FILE="$DIR/../docker/docker-compose.yml"
 TEST_FILE="$DIR/../smoketest/smoketest.py"
 
 cat docker/addresses.json
-docker-compose -f "$COMPOSE_FILE" up -d
+docker-compose -f "$COMPOSE_FILE" pull
+docker-compose -f "$COMPOSE_FILE" build
+docker-compose -f "$COMPOSE_FILE" start
 sleep 60
+docker-compose -f "$COMPOSE_FILE" ps
 docker-compose -f "$COMPOSE_FILE" logs | cat
 cat docker/addresses.json
 python -m pytest $TEST_FILE
