0b693b1778ae7d2956f4f7f5aa1b027ab4f50cf8
Bernd Bohmeier <berndbohmeier@googlemail.com>
2018-04-08 23:05:47 +0200

    Add some test output to smoketest

--- .travis/smoketest.sh
+++ .travis/smoketest.sh
@@ -4,7 +4,10 @@ DIR="$(dirname "$(readlink -f "$0")")"
 COMPOSE_FILE="$DIR/../docker/docker-compose.yml"
 TEST_FILE="$DIR/../smoketest/smoketest.py"
 
+cat docker/addresses.json
 docker-compose -f "$COMPOSE_FILE" up -d
 sleep 30
+docker-compose -f "$COMPOSE_FILE" logs | cat
+cat docker/addresses.json
 python -m pytest $TEST_FILE
 docker-compose -f "$COMPOSE_FILE" down
