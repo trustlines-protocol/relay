2cf85a88e4189af2721c1d570a8646c69db0e116
Dong-Ha Kim <dongha.kim210@gmail.com>
2018-06-26 17:06:45 +0200

    change parent of ExchangeEvent to TLNetworkEvent

--- relay/blockchain/exchange_events.py
+++ relay/blockchain/exchange_events.py
@@ -1,49 +1,21 @@
 from eth_utils import force_bytes
-from .events import BlockchainEvent
-
+from .events import TLNetworkEvent, BlockchainEvent  # NOQA
 
 LogFillEventType = 'LogFill'
 LogCancelEventType = 'LogCancel'
 
 
-class ExchangeEvent(BlockchainEvent):
+class ExchangeEvent(TLNetworkEvent):
 
     def __init__(self, web3_event, current_blocknumber, timestamp, user=None):
-        super().__init__(web3_event, current_blocknumber, timestamp)
-        self.user = user
+        super().__init__(web3_event, current_blocknumber, timestampm, from_to_types, user)
         self.exchange_address = web3_event.get('address')
         self.order_hash = force_bytes(web3_event.get('args').get('orderHash'))
         self.maker_token = web3_event.get('args').get('makerToken')
         self.taker_token = web3_event.get('args').get('takerToken')
 
-    @property
-    def from_(self):
-        return self._web3_event.get('args')[from_to_types[self._web3_event.get('event')][0]]
-
 
 class LogFillEvent(ExchangeEvent):
-
-    @property
-    def to(self):
-        return self._web3_event.get('args')[from_to_types[self._web3_event.get('event')][1]]
-
-    @property
-    def direction(self):
-        if self.user is None:
-            return None
-        if self.from_ == self.user:
-            return 'sent'
-        else:
-            return 'received'
-
-    @property
-    def other_party(self):
-        if self.user is None:
-            return None
-        if self.from_ == self.user:
-            return self.to
-        else:
-            return self.from_
 
     @property
     def filled_maker_amount(self):
@@ -73,5 +45,5 @@ event_builders = {
 
 from_to_types = {
     LogFillEventType: ['maker', 'taker'],
-    LogCancelEventType: ['maker']
+    LogCancelEventType: ['maker', 'maker']
 }
