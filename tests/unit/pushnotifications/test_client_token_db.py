33fda9eb6e99f9e469fe2686cec98ddc9d4c1411
Bernd Bohmeier <berndbohmeier@googlemail.com>
2018-06-29 11:50:41 +0200

    Add simple database for client tokens

---
+++ tests/unit/pushnotifications/test_client_token_db.py
@@ -0,0 +1,35 @@
+import pytest
+
+from sqlalchemy import create_engine
+
+from relay.pushservice.client_token_db import ClientTokenDB, ClientTokenAlreadyExistsException
+
+
+@pytest.fixture()
+def client_token_db():
+    return ClientTokenDB(create_engine('sqlite:///:memory:'))
+
+
+def test_add_client_token(client_token_db: ClientTokenDB):
+    client_token_db.add_client_token('0x123', 'token')
+    assert client_token_db.get_client_tokens('0x123') == ['token']
+
+
+def test_add_multiple_client_token(client_token_db: ClientTokenDB):
+    client_token_db.add_client_token('0x123', 'token1')
+    client_token_db.add_client_token('0x123', 'token2')
+    client_token_db.add_client_token('0x123', 'token3')
+    assert set(client_token_db.get_client_tokens('0x123')) == {'token1', 'token2', 'token3'}
+
+
+def test_cannot_add_same_token_twice(client_token_db: ClientTokenDB):
+    client_token_db.add_client_token('0x123', 'token')
+    with pytest.raises(ClientTokenAlreadyExistsException):
+        client_token_db.add_client_token('0x123', 'token')
+    assert client_token_db.get_client_tokens('0x123') == ['token']
+
+
+def test_delete_token(client_token_db: ClientTokenDB):
+    client_token_db.add_client_token('0x123', 'token')
+    client_token_db.delete_client_token('0x123', 'token')
+    assert client_token_db.get_client_tokens('0x123') == []
