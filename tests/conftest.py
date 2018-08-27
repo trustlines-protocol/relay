7ae7b13032d14565c83a1a567c8772a82c36d823
Ralf Schmitt <ralf@systemexit.de>
2018-08-27 15:36:46 +0200

    add tester fixture
    this fixture provides tester.k0, tester.a0 compatible to the
    ethereum.tools.tester namespace.
    
    We need to fund the a0 address for some tests later. test_signing will work with
    this one, and we eventually can get rid of pyethereum.

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,3 +22,14 @@ def addresses() -> Sequence[str]:
         '0x57Dd8AC67427E8B270B9C15dEDd8B2501a8F7Fee',
         '0xea571341F70B2fE15716e494d1fF95A47d1cDc0E'
     ]
+
+
+@pytest.fixture()
+def tester():
+    """ethereum.tools.tester compatible class"""
+    class tester:
+        pass
+    t = tester()
+    t.k0 = b'\x04HR\xb2\xa6p\xad\xe5@~x\xfb(c\xc5\x1d\xe9\xfc\xb9eB\xa0q\x86\xfe:\xed\xa6\xbb\x8a\x11m'
+    t.a0 = b'\x82\xa9x\xb3\xf5\x96*[\tW\xd9\xee\x9e\xefG.\xe5[B\xf1'
+    return t
