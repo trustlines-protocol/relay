9c83a7b36ea80b65b6e1d55ce6c6658bec5fa8b1
cducrest <come.ducrest@gmail.com>
2018-12-28 14:41:21 +0100

    Cleaning up and refactoring

--- relay/network_graph/fees.py
+++ relay/network_graph/fees.py
@@ -19,78 +19,43 @@ def imbalance_generated(*, value, balance):
     return max(value - balance, 0)
 
 
-def estimate_max_fee_from_max_imbalance(divisor, imbalance):
-    """Gives a higher limit on the possible fee on a transfer bringing imbalance so that
-    fee_actual(imbalance - fee_estimation) <= fee_estimation
-    this function should estimate what needs to be substracted to imbalance to make it sendable"""
-
-    # TODO: either improve this function or remove it and use calculate_fees_reverse
-
-    if (imbalance <= 0):
-        fee_estimation = 0
-    else:
-        # fee_estimation = (imbalance - 1) // (divisor - 1) + 1  # fees you would pay if you sent imbalance
-        fee_estimation = (imbalance - 1) // divisor + 1
-        # the error is that you should not pay fees on transferring the fees.
-
-    # fee_estimation -= fee_estimation // divisor  # this is wrong
-    # what needs to be removed is not the fee on fee_estimation but the fees on fee_actual
-
-    # fee_estimation = int((1+capacity/divisor)/(1+1/divisor))
-    # max_sendable = capacity - fee_estimation
-
-    return fee_estimation
-
-
-def estimate_sendable_from_one_limiting_capacity(divisor, capacity, balance, number_of_hops_to_destination):
+def estimate_sendable_from_one_limiting_capacity(divisor, capacity, balance, number_of_hops_to_target):
     """Estimates the max sendable amount in a path where the limiting capacity is assumed to be in a single trustline
-    with given capacity, balance, and that is number_of_hops_to_destination away from the destination"""
+    with given capacity, balance, and that is number_of_hops_to_target away from the target"""
 
     if divisor == 0:
         return capacity
 
-    if number_of_hops_to_destination <= 0:
+    if number_of_hops_to_target == 0:
         return capacity
 
     imbalance = imbalance_generated(value=capacity, balance=balance)
-    fee_estimation = estimate_max_fee_from_max_imbalance(divisor, imbalance)
+    fee_estimation = calculate_fees(imbalance, divisor)
 
     max_sendable = capacity - fee_estimation
 
     return estimate_sendable_from_one_limiting_capacity(divisor,
                                                         max_sendable,
                                                         balance,
-                                                        number_of_hops_to_destination - 1)
+                                                        number_of_hops_to_target - 1)
 
 
 def estimate_sendable(divisor, path_capacities, path_balances):
-    """Estimates the max sendable amount in a path with given capacities and balances
-    the estimated value returned is lower than the actual maximum sendable amount"""
+    """Estimates the max sendable amount in a path with given capacities and balances"""
 
-    number_of_hops_to_destination = len(path_capacities)-1
+    number_of_hops_to_target = len(path_capacities) - 1
+    min_sendable = float('inf')
 
-    min_sendable = estimate_sendable_from_one_limiting_capacity(divisor,
-                                                                path_capacities[len(path_capacities)-1],
-                                                                path_balances[len(path_capacities)-1],
-                                                                0)
-
-    for i in range(number_of_hops_to_destination):
-
-        print("i = ", i)
-        print("capacity", path_capacities[i])
+    for i in range(len(path_capacities)):
 
         current_sendable = estimate_sendable_from_one_limiting_capacity(divisor,
                                                                         path_capacities[i],
                                                                         path_balances[i],
-                                                                        number_of_hops_to_destination)
-        print("current_sendable", current_sendable)
-        print("number_of_hops_to_dest", number_of_hops_to_destination)
+                                                                        number_of_hops_to_target)
 
         if current_sendable < min_sendable:
             min_sendable = current_sendable
 
-
-
-        number_of_hops_to_destination -= 1
+        number_of_hops_to_target -= 1
 
     return min_sendable
--- tests/unit/network_graph/test_fees.py
+++ tests/unit/network_graph/test_fees.py
@@ -2,7 +2,6 @@ from relay.network_graph.fees import (
     calculate_fees,
     calculate_fees_reverse,
     imbalance_generated,
-    estimate_max_fee_from_max_imbalance,
 )
 
 
@@ -38,54 +37,3 @@ def test_calculate_fees_reverse(CalculateFee):
         calculate_fees_reverse(imbalance_generated, capacity_imbalance_fee_divisor)
         == expected_fees_reverse
     )
-
-
-def test_estimate_fees_from_capacity_single_hop():
-    """
-    Tests the estimation for a single hop
-    The estimation has to be an upper bound so the actual fees have to be lower
-    """
-    imbalance = 12345
-    fee_estimation = estimate_max_fee_from_max_imbalance(100, imbalance)
-    fee_actual = calculate_fees(imbalance - fee_estimation, 100)
-    assert fee_estimation >= fee_actual
-
-
-def test_estimate_max_fees_from_capacity_single_hop_upper_edge_case():
-    """Tests the upper value of the edge case for the estimation"""
-    imbalance = 101
-    fee_estimation = estimate_max_fee_from_max_imbalance(100, imbalance)
-    fee_actual = calculate_fees(imbalance - fee_estimation, 100)
-    assert fee_estimation >= fee_actual
-
-
-def test_estimate_max_fees_from_capacity_single_hop_lower_edge_case():
-    """Tests the lower value of the edge case for the estimation"""
-    imbalance = 100
-    fee_estimation = estimate_max_fee_from_max_imbalance(100, imbalance)
-    fee_actual = calculate_fees(imbalance - fee_estimation, 100)
-    assert fee_estimation >= fee_actual
-
-
-def test_estimate_max_fees_from_capacity_single_hop_smaller_than_divisor():
-    """Tests the fee estimation with a value smaller than the divisor"""
-    imbalance = 42
-    fee_estimation = estimate_max_fee_from_max_imbalance(100, imbalance)
-    fee_actual = calculate_fees(imbalance - fee_estimation, 100)
-    assert fee_estimation >= fee_actual
-
-
-def test_estimate_max_fees_from_capacity_single_hop_sanity():
-    """Tests whether for small values outside of indeterminate case, the estimation is exact"""
-    imbalance = 150
-    fee_estimation = estimate_max_fee_from_max_imbalance(100, imbalance)
-    fee_actual = calculate_fees(imbalance - fee_estimation, 100)
-    assert fee_estimation >= fee_actual
-
-
-def test_estimate_max_fees_from_capacity_single_hop_high_value():
-    """Tests whether for small values outside of indeterminate case, the estimation is exact"""
-    imbalance = 123456789
-    fee_estimation = estimate_max_fee_from_max_imbalance(100, imbalance)
-    fee_actual = calculate_fees(imbalance - fee_estimation, 100)
-    assert fee_estimation >= fee_actual
