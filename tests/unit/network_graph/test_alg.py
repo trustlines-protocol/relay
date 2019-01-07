09ac00f4f43d13851da5c7637e8b36fc03f859c7
Ralf Schmitt <ralf@systemexit.de>
2019-01-07 16:28:30 +0100

    add test case that show unneccessary calls to cost fn
    Here's a failing test case for our least cost function. Currently we do call the
    costfn for some hops where we already found the shortest path to, i.e. these
    calls are not necessary.
    
    See https://github.com/trustlines-network/relay/issues/237

---
+++ tests/unit/network_graph/test_alg.py
@@ -0,0 +1,36 @@
+import networkx as nx
+from relay.network_graph import alg
+
+
+class FeeCostAccumulatorCounter(alg.CostAccumulator):
+    def __init__(self):
+        self.num_calls = 0
+
+    def zero(self):
+        return 0
+
+    def total_cost_from_start_to_dst(
+        self, cost_from_start_to_node, node, dst, graph_data
+    ):
+        self.num_calls += 1
+        return cost_from_start_to_node + graph_data["fee"]
+
+
+def test_optimize_number_of_costfn_calls():
+    """test that least_cost_path does not call costfn excessively
+
+    see https://github.com/trustlines-network/relay/issues/237
+    """
+    g = nx.Graph()
+    nodes = list(range(1, 20))
+    for src, dst in zip(nodes, nodes[1:]):
+        g.add_edge(src, dst, fee=1)
+
+    cost_accumulator = FeeCostAccumulatorCounter()
+    alg.least_cost_path(
+        graph=g,
+        starting_nodes={nodes[0]},
+        target_nodes={nodes[-1]},
+        cost_accumulator=cost_accumulator,
+    )
+    assert cost_accumulator.num_calls == len(nodes) - 1
