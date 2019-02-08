8c2ac493f7c39125796a767d00a5a7b030edfc01
Bernd Bohmeier <berndbohmeier@googlemail.com>
2019-02-08 18:11:15 +0100

    Use HexBytes in meta transaction schema
    Use HexBytes in meta transaction schema for consistency
    Extend the meta transaction schema tests to check for problems
    with using hexbytes
    Better test for valid and not valid values in meta transaction

--- relay/api/schemas.py
+++ relay/api/schemas.py
@@ -14,27 +14,27 @@ class MetaTransactionSchema(Schema):
         strict = True
 
     def _validate(self, data):
-        nonce = data["nonce"]
+        nonce = data['nonce']
         if not 0 <= nonce < 2 ** 256:
-            raise ValidationError(f"nonce={nonce} is out of bounds")
-        if len(data["signature"]) != 65:
-            raise ValidationError("signature must be 65 bytes")
+            raise ValidationError(f'nonce={nonce} is out of bounds')
+        if len(data['signature']) != 65:
+            raise ValidationError('signature must be 65 bytes')
 
-        value = data["value"]
+        value = data['value']
         if not 0 <= value < 2 ** 256:
-            raise ValidationError(f"value={value} is out of bounds")
+            raise ValidationError(f'value={value} is out of bounds')
 
     @post_load
     def make_meta_transaction(self, data):
         self._validate(data)
         return identity.MetaTransaction(**data)
 
-    from_ = Address(required=True, dump_to="from", load_from="from")
+    from_ = Address(required=True, dump_to='from', load_from='from')
     to = Address(required=True)
     value = BigInteger(required=True)
     data = HexEncodedBytes(required=True)
     nonce = BigInteger(required=True)
-    extraData = HexEncodedBytes(required=True, attribute="extra_data")
+    extraData = HexEncodedBytes(required=True, attribute='extra_data')
     signature = HexEncodedBytes(required=True)
 
 
@@ -50,55 +50,55 @@ class MessageEventSchema(EventSchema):
 
 
 class BlockchainEventSchema(EventSchema):
-    blockNumber = fields.Integer(attribute="blocknumber")
-    type = fields.Str(default="event")
-    transactionId = HexBytes(attribute="transaction_id")
+    blockNumber = fields.Integer(attribute='blocknumber')
+    type = fields.Str(default='event')
+    transactionId = HexBytes(attribute='transaction_id')
     status = fields.Str()
 
 
 class CurrencyNetworkEventSchema(BlockchainEventSchema):
-    networkAddress = Address(attribute="network_address")
-    amount = BigInteger(attribute="value")
-    given = BigInteger(attribute="creditline_given")
-    received = BigInteger(attribute="creditline_received")
+    networkAddress = Address(attribute='network_address')
+    amount = BigInteger(attribute='value')
+    given = BigInteger(attribute='creditline_given')
+    received = BigInteger(attribute='creditline_received')
     balance = BigInteger()
-    interestRateGiven = BigInteger(attribute="interest_rate_given")
-    interestRateReceived = BigInteger(attribute="interest_rate_received")
-    leftGiven = BigInteger(attribute="left_given")
-    leftReceived = BigInteger(attribute="left_received")
-    from_ = Address(dump_to="from", load_from="from")
+    interestRateGiven = BigInteger(attribute='interest_rate_given')
+    interestRateReceived = BigInteger(attribute='interest_rate_received')
+    leftGiven = BigInteger(attribute='left_given')
+    leftReceived = BigInteger(attribute='left_received')
+    from_ = Address(dump_to='from', load_from='from')
     to = Address()
 
 
 class UserCurrencyNetworkEventSchema(CurrencyNetworkEventSchema):
     direction = fields.Str()
-    counterParty = Address(attribute="counter_party")
+    counterParty = Address(attribute='counter_party')
     user = Address()
 
 
 class TokenEventSchema(BlockchainEventSchema):
-    tokenAddress = Address(attribute="token_address")
-    amount = BigInteger(attribute="value")
-    from_ = Address(dump_to="from", load_from="from")
+    tokenAddress = Address(attribute='token_address')
+    amount = BigInteger(attribute='value')
+    from_ = Address(dump_to='from', load_from='from')
     to = Address()
 
 
 class UserTokenEventSchema(TokenEventSchema):
     direction = fields.Str()
-    counterParty = Address(attribute="counter_party")
+    counterParty = Address(attribute='counter_party')
     user = Address()
 
 
 class ExchangeEventSchema(BlockchainEventSchema):
-    exchangeAddress = Address(attribute="exchange_address")
-    makerTokenAddress = Address(attribute="maker_token")
-    takerTokenAddress = Address(attribute="taker_token")
-    from_ = Address(dump_to="from", load_from="from")
-    orderHash = HexBytes(attribute="order_hash")
-    filledMakerAmount = BigInteger(attribute="filled_maker_amount")
-    filledTakerAmount = BigInteger(attribute="filled_taker_amount")
-    cancelledMakerAmount = BigInteger(attribute="cancelled_maker_amount")
-    cancelledTakerAmount = BigInteger(attribute="cancelled_taker_amount")
+    exchangeAddress = Address(attribute='exchange_address')
+    makerTokenAddress = Address(attribute='maker_token')
+    takerTokenAddress = Address(attribute='taker_token')
+    from_ = Address(dump_to='from', load_from='from')
+    orderHash = HexBytes(attribute='order_hash')
+    filledMakerAmount = BigInteger(attribute='filled_maker_amount')
+    filledTakerAmount = BigInteger(attribute='filled_taker_amount')
+    cancelledMakerAmount = BigInteger(attribute='cancelled_maker_amount')
+    cancelledTakerAmount = BigInteger(attribute='cancelled_taker_amount')
     to = Address()
 
 
@@ -108,32 +108,32 @@ class UserExchangeEventSchema(ExchangeEventSchema):
 
 class AnyEventSchema(OneOfSchema):
     type_schemas = {
-        "CurrencyNetworkEvent": UserCurrencyNetworkEventSchema,
-        "UnwEthEvent": UserTokenEventSchema,
-        "ExchangeEvent": ExchangeEventSchema,
+        'CurrencyNetworkEvent': UserCurrencyNetworkEventSchema,
+        'UnwEthEvent': UserTokenEventSchema,
+        'ExchangeEvent': ExchangeEventSchema,
     }
 
-    type_field = "__class__"
+    type_field = '__class__'
 
     def get_obj_type(self, obj):
         if isinstance(obj, CurrencyNetworkEvent):
-            return "CurrencyNetworkEvent"
+            return 'CurrencyNetworkEvent'
         elif isinstance(obj, UnwEthEvent):
-            return "UnwEthEvent"
+            return 'UnwEthEvent'
         elif isinstance(obj, ExchangeEvent):
-            return "ExchangeEvent"
+            return 'ExchangeEvent'
 
-        raise RuntimeError(f"Unknown object type: {obj.__class__.__name__}")
+        raise RuntimeError(f'Unknown object type: {obj.__class__.__name__}')
 
 
 class AccountSummarySchema(Schema):
     class Meta:
         strict = True
 
-    leftGiven = BigInteger(attribute="creditline_left_given")
-    leftReceived = BigInteger(attribute="creditline_left_received")
-    given = BigInteger(attribute="creditline_given")
-    received = BigInteger(attribute="creditline_received")
+    leftGiven = BigInteger(attribute='creditline_left_given')
+    leftReceived = BigInteger(attribute='creditline_left_received')
+    given = BigInteger(attribute='creditline_given')
+    received = BigInteger(attribute='creditline_received')
     balance = BigInteger()
 
 
@@ -141,12 +141,12 @@ class TrustlineSchema(Schema):
     class Meta:
         strict = True
 
-    leftGiven = BigInteger(attribute="creditline_left_given")
-    leftReceived = BigInteger(attribute="creditline_left_received")
-    interestRateGiven = BigInteger(attribute="interest_rate_given")
-    interestRateReceived = BigInteger(attribute="interest_rate_received")
-    given = BigInteger(attribute="creditline_given")
-    received = BigInteger(attribute="creditline_received")
+    leftGiven = BigInteger(attribute='creditline_left_given')
+    leftReceived = BigInteger(attribute='creditline_left_received')
+    interestRateGiven = BigInteger(attribute='interest_rate_given')
+    interestRateReceived = BigInteger(attribute='interest_rate_received')
+    given = BigInteger(attribute='creditline_given')
+    received = BigInteger(attribute='creditline_received')
     balance = BigInteger()
     user = Address()
     counterParty = Address()
@@ -160,29 +160,29 @@ class TxInfosSchema(Schema):
 
     balance = BigInteger()
     nonce = fields.Integer()
-    gasPrice = BigInteger(attribute="gas_price")
+    gasPrice = BigInteger(attribute='gas_price')
 
 
 class CurrencyNetworkSchema(Schema):
     class Meta:
         strict = True
 
-    abbreviation = fields.Str(attribute="symbol")
+    abbreviation = fields.Str(attribute='symbol')
     name = fields.Str()
     address = Address()
     decimals = fields.Int()
-    numUsers = fields.Int(attribute="num_users")
-    defaultInterestRate = BigInteger(attribute="default_interest_rate")
-    interestRateDecimals = fields.Int(attribute="interest_rate_decimals")
-    customInterests = fields.Bool(attribute="custom_interests")
-    preventMediatorInterests = fields.Bool(attribute="prevent_mediator_interests")
+    numUsers = fields.Int(attribute='num_users')
+    defaultInterestRate = BigInteger(attribute='default_interest_rate')
+    interestRateDecimals = fields.Int(attribute='interest_rate_decimals')
+    customInterests = fields.Bool(attribute='custom_interests')
+    preventMediatorInterests = fields.Bool(attribute='prevent_mediator_interests')
 
 
 class PaymentPathSchema(Schema):
     class Meta:
         strict = True
 
-    fees = BigInteger(required=True, attribute="fee")
+    fees = BigInteger(required=True, attribute='fee')
     path = fields.List(Address(), required=True)
-    estimatedGas = BigInteger(attribute="estimated_gas")
+    estimatedGas = BigInteger(attribute='estimated_gas')
     value = BigInteger()
