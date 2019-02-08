b0b54d1739ec5058075411c667c3abde3aae6890
Ralf Schmitt <ralf@systemexit.de>
2019-02-08 16:49:21 +0100

    add some additional validations for MetaTransactionSchema
    I wasn't able to use the marshmallow.validates_schema, so we just call our
    _validate method explicitly from the post_load function.

--- tests/unit/api/test_schemas.py
+++ tests/unit/api/test_schemas.py
@@ -5,23 +5,27 @@ import pytest
 import attr
 from tldeploy import identity
 from relay.api import schemas
+from marshmallow import ValidationError
+
+
+a_valid_meta_transaction = identity.MetaTransaction(
+    from_="0xF2E246BB76DF876Cef8b38ae84130F4F55De395b",
+    to="0x51a240271AB8AB9f9a21C82d9a85396b704E164d",
+    value=0,
+    data=bytes.fromhex(
+        "46432830000000000000000000000000000000000000000000000000000000000000000a"
+    ),
+    nonce=1,
+    extra_data=b"",
+    signature=bytes.fromhex(
+        "6d2fe56ef6648cb3f0398966ad3b05d891cde786d8074bdac15bcb92ebfa722248"
+        "9b8eb6ed87165feeede19b031bb69e12036a5fa13b3a46ad0c2c19d051ea9101"
+    ),
+)
 
 
 def gen_meta_transactions():
-    mt = identity.MetaTransaction(
-        from_="0xF2E246BB76DF876Cef8b38ae84130F4F55De395b",
-        to="0x51a240271AB8AB9f9a21C82d9a85396b704E164d",
-        value=0,
-        data=bytes.fromhex(
-            "46432830000000000000000000000000000000000000000000000000000000000000000a"
-        ),
-        nonce=1,
-        extra_data=b"",
-        signature=bytes.fromhex(
-            "6d2fe56ef6648cb3f0398966ad3b05d891cde786d8074bdac15bcb92ebfa722248"
-            "9b8eb6ed87165feeede19b031bb69e12036a5fa13b3a46ad0c2c19d051ea9101"
-        ),
-    )
+    mt = a_valid_meta_transaction
     return [
         mt,
         attr.evolve(mt, data=b""),
@@ -41,3 +45,23 @@ def test_meta_transaction_schema(meta_transaction):
     print("LOADED", loaded)
 
     assert loaded == meta_transaction
+
+
+@pytest.mark.parametrize(
+    "values",
+    [
+        dict(nonce="-1"),
+        dict(nonce=str(2 ** 256)),
+        dict(value="-1"),
+        dict(value=str(2 ** 256)),
+        dict(value="1.5"),
+        dict(signature=""),
+        dict(signature="x" * 64),
+        dict(signature="x" * 66),
+    ],
+)
+def test_extended_validation(values):
+    dumped = schemas.MetaTransactionSchema().dump(a_valid_meta_transaction).data
+    dumped.update(values)
+    with pytest.raises(ValidationError):
+        schemas.MetaTransactionSchema().load(dumped)
