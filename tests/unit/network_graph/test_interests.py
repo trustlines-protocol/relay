eb9c114b6d9ec9b0818494b53370b42cb7f11d23
cducrest <come.ducrest@gmail.com>
2019-02-14 14:56:17 +0100

    Add function for find_path receiver pays for fees. Modify tests so that the paths tests run with both find_path functions. Add test for fees of two find_path functions

--- tests/unit/network_graph/test_interests.py
+++ tests/unit/network_graph/test_interests.py
@@ -172,7 +172,7 @@ def test_interests_path_from_A_balance_positive_relevant_interests(
 ):
     # B owes to A
     # 1% interest given by A to B
-    cost, path = configurable_community.find_path(A, B, 100, timestamp=SECONDS_PER_YEAR)
+    cost, path = configurable_community.find_path_sender_pays_fees(A, B, 100, timestamp=SECONDS_PER_YEAR)
     assert path == [A, B]
 
 
@@ -194,7 +194,7 @@ def test_interests_path_from_A_balance_negative_relevant_interests(
 ):
     # A owes to B
     # 1% interest given by B to A
-    cost, path = configurable_community.find_path(A, B, 100, timestamp=SECONDS_PER_YEAR)
+    cost, path = configurable_community.find_path_sender_pays_fees(A, B, 100, timestamp=SECONDS_PER_YEAR)
     assert path == []
 
 
@@ -217,7 +217,7 @@ def test_interests_path_from_A_balance_positive_irrelevant_interests(
     # B owes to A
     # 1% interest given by B to A
 
-    cost, path = configurable_community.find_path(A, B, 100, timestamp=SECONDS_PER_YEAR)
+    cost, path = configurable_community.find_path_sender_pays_fees(A, B, 100, timestamp=SECONDS_PER_YEAR)
     assert path == [A, B]
 
 
@@ -239,7 +239,7 @@ def test_interests_path_from_A_balance_negative_irrelevant_interests(
 ):
     # A owes to B
     # 1% interest given by A to B
-    cost, path = configurable_community.find_path(A, B, 100, timestamp=SECONDS_PER_YEAR)
+    cost, path = configurable_community.find_path_sender_pays_fees(A, B, 100, timestamp=SECONDS_PER_YEAR)
     assert path == [A, B]
 
 
@@ -261,7 +261,7 @@ def test_interests_path_from_B_balance_positive_relevant_interests(
 ):
     # B owes to A
     # 1% interest given by A to B
-    cost, path = configurable_community.find_path(B, A, 100, timestamp=SECONDS_PER_YEAR)
+    cost, path = configurable_community.find_path_sender_pays_fees(B, A, 100, timestamp=SECONDS_PER_YEAR)
     assert path == []
 
 
@@ -283,7 +283,7 @@ def test_interests_path_from_B_balance_negative_relevant_interests(
 ):
     # A owes to B
     # 1% interest given by B to A
-    cost, path = configurable_community.find_path(B, A, 100, timestamp=SECONDS_PER_YEAR)
+    cost, path = configurable_community.find_path_sender_pays_fees(B, A, 100, timestamp=SECONDS_PER_YEAR)
     assert path == [B, A]
 
 
@@ -305,7 +305,7 @@ def test_interests_path_from_B_balance_positive_irrelevant_interests(
 ):
     # B owes to A
     # 1% interest given by B to A
-    cost, path = configurable_community.find_path(B, A, 100, timestamp=SECONDS_PER_YEAR)
+    cost, path = configurable_community.find_path_sender_pays_fees(B, A, 100, timestamp=SECONDS_PER_YEAR)
     assert path == [B, A]
 
 
@@ -327,5 +327,5 @@ def test_interests_path_from_B_balance_negative_irrelevant_interests(
 ):
     # A owes to B
     # 1% interest given by A to B
-    cost, path = configurable_community.find_path(B, A, 100, timestamp=SECONDS_PER_YEAR)
+    cost, path = configurable_community.find_path_sender_pays_fees(B, A, 100, timestamp=SECONDS_PER_YEAR)
     assert path == [B, A]
