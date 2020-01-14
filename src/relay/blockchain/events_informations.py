df503902070f2eeb757c2049de50de23da8a853a
cducrest <come.ducrest@gmail.com>
2020-01-14 18:03:06 +0100

    Refactor list comprehension to loop in get_list_of_paid_interests
    This makes it easier to understand.
    Also change InterestAccrued from namedtuple to attr.s class

--- src/relay/blockchain/events_informations.py
+++ src/relay/blockchain/events_informations.py
@@ -1,18 +1,24 @@
 import math
-from collections import namedtuple
 from typing import List
 
+import attr
+import toolz
+
 from relay.blockchain.currency_network_events import (
-    BalanceUpdateEvent,
     BalanceUpdateEventType,
     TrustlineUpdateEventType,
 )
 from relay.network_graph.interests import calculate_interests
 
-InterestAccrued = namedtuple("InterestAccrued", ["value", "interest_rate", "timestamp"])
-
 DIRECTION_SENT = "sent"
 DIRECTION_RECEIVED = "received"
+
+
+@attr.s
+class InterestAccrued:
+    value = attr.ib()
+    interest_rate = attr.ib()
+    timestamp = attr.ib()
 
 
 def get_list_of_paid_interests_for_trustline(
@@ -31,44 +37,33 @@ def get_list_of_paid_interests_for_trustline(
         event_name=TrustlineUpdateEventType,
     )
 
-    timestamps = [
-        balance_update_event.timestamp for balance_update_event in balance_update_events
-    ]
-
-    balances = get_sorted_balances_from_update_events(balance_update_events)
-    interest_rates = [
-        get_interests_rates_of_trustline_for_user_before_timestamp(
-            trustline_update_events, balance, timestamp
+    accrued_interests = []
+    for (pre_balance_event, post_balance_event) in toolz.itertoolz.sliding_window(
+        2, balance_update_events
+    ):
+        balance = balance_viewed_from_user(pre_balance_event)
+        interest_rate = get_interests_rates_of_trustline_for_user_before_timestamp(
+            trustline_update_events, balance, post_balance_event.timestamp
         )
-        for (balance, timestamp) in zip(balances[:-1], timestamps[1:])
-    ]
-
-    return [
-        InterestAccrued(
-            calculate_interests(balance, interest_rates, post_time - pre_time),
-            interest_rates,
-            post_time,
+        interest_value = calculate_interests(
+            balance,
+            interest_rate,
+            post_balance_event.timestamp - pre_balance_event.timestamp,
         )
-        for (balance, interest_rates, pre_time, post_time) in zip(
-            balances[:-1], interest_rates, timestamps[:-1], timestamps[1:]
+        accrued_interests.append(
+            InterestAccrued(interest_value, interest_rate, post_balance_event.timestamp)
         )
-    ]
+
+    return accrued_interests
 
 
-def get_sorted_balances_from_update_events(
-    balance_update_events: List[BalanceUpdateEvent]
-):
-    """Get all balances of a trustline in sorted order from the view of user"""
-
-    def balance_viewed_from_user(balance_update_event):
-        if balance_update_event.direction == DIRECTION_SENT:
-            return balance_update_event.value
-        elif balance_update_event.direction == DIRECTION_RECEIVED:
-            return -balance_update_event.value
-        else:
-            raise RuntimeError("Unexpected balance update event")
-
-    return [balance_viewed_from_user(event) for event in balance_update_events]
+def balance_viewed_from_user(balance_update_event):
+    if balance_update_event.direction == DIRECTION_SENT:
+        return balance_update_event.value
+    elif balance_update_event.direction == DIRECTION_RECEIVED:
+        return -balance_update_event.value
+    else:
+        raise RuntimeError("Unexpected balance update event")
 
 
 def get_interests_rates_of_trustline_for_user_before_timestamp(
