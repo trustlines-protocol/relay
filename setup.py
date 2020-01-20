583be04e0fb1a9be0cbecb303e7e11e2e5386605
Ralf Schmitt <ralf@brainbot.com>
2020-01-20 13:37:15 +0100

    Require python 3.6 in setup.py
    Also get rid of the inline comments; this is not a python packaging
    tutorial.

--- setup.py
+++ setup.py
@@ -1,29 +1,16 @@
-"""A setuptools based setup module.
+import codecs
+import os
 
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
-
-# To use a consistent encoding
-from codecs import open
-from os import path
-
-# Always prefer setuptools over distutils
 from setuptools import find_packages, setup
 
-here = path.abspath(path.dirname(__file__))
+here = os.path.abspath(os.path.dirname(__file__))
 
-# Get the long description from the README file
-with open(path.join(here, "README.rst"), encoding="utf-8") as f:
+with codecs.open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
 
 setup(
     name="trustlines-relay",
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
     setup_requires=["setuptools_scm"],
     use_scm_version=True,
     description="Relay Server for the Trustlines Network",
@@ -33,7 +20,6 @@ setup(
     # Author details
     author="Trustlines-Network",
     author_email="contact@brainbot.com",
-    # Choose your license
     license="MIT",
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -53,17 +39,8 @@ setup(
     ],
     # What does your project relate to?
     keywords="trustlines",
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
     packages=find_packages("src"),
     package_dir={"": "src"},
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
         "flask",
         "flask_restful",
@@ -91,23 +68,6 @@ setup(
         "toml",
         "cachetools",
     ],
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    extras_require={},
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    package_data={},
-    python_requires=">=3",
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[],
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
+    python_requires=">=3.6",
     entry_points={"console_scripts": ["tl-relay=relay.main:main"]},
 )
