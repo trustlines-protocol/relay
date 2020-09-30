112a1d64a8cb085827bbf0f1d71865ddc8a3e3c8
cducrest <come.ducrest@gmail.com>
2020-09-30 14:45:40 +0200

    Update web3 to 5.7.0 to be compatible with new contract deploy tools

--- src/relay/web3provider.py
+++ src/relay/web3provider.py
@@ -2,6 +2,7 @@ import logging
 from enum import Enum
 from typing import MutableMapping
 
+from eth_typing import URI
 from web3 import HTTPProvider, IPCProvider, WebsocketProvider
 from web3.providers.auto import load_provider_from_uri
 
@@ -30,7 +31,7 @@ def create_provider_from_config(rpc_config: MutableMapping):
             rpc_config["port"],
         )
         logger.info("Using HTTP provider with URL {}".format(url))
-        return HTTPProvider(url)
+        return HTTPProvider(URI(url))
     elif provider_type is ProviderType.WEBSOCKET:
         url = "{}://{}:{}".format(
             "wss" if rpc_config["use_ssl"] else "ws",
@@ -38,7 +39,7 @@ def create_provider_from_config(rpc_config: MutableMapping):
             rpc_config["port"],
         )
         logger.info("Using websocket provider with URL {}".format(url))
-        return WebsocketProvider(url)
+        return WebsocketProvider(URI(url))
     elif provider_type is ProviderType.IPC:
         file_path = rpc_config["file_path"]
         logger.info("Using IPC provider with file path {}".format(file_path))
